204249f6b3eb412340917425ad1cf4730a134662
ephong93 <ephong93@gmail.com>
2021-08-16 01:14:08 +0900

    Add backend

--- front/craco.config.js
+++ front/craco.config.js
@@ -11,10 +11,7 @@ module.exports = {
                             '@primary-color': '#1DA57A',
                             '@layout-body-background': '#fefefe',
                             '@layout-header-background': '#1DA57A',
-                            //'@list-item-padding': 0,
-                            //'@list-empty-text-padding': 0,
-                            //'@list-item-padding-sm': 0,
-                            //'@list-item-padding-lg': 0
+                            '@list-item-padding': '@padding-xs @padding-sm'
                         },
                         javascriptEnabled: true,
                     }
--- front/src/App.js
+++ front/src/App.js
@@ -2,6 +2,7 @@ import Main from './pages/Main';
 import './App.less';
 
 
+
 function App() {
   return (
     <div className="App">
